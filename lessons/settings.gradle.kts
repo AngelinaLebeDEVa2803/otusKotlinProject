dafa8f44d868cf5d0bd302c9e86963e867bf4887
AngelinaLebeDEVa2803 <lebedevaangelina2803@gmail.com>
2025-07-19 21:13:15 +0300

    Composite

--- lessons/settings.gradle.kts
+++ lessons/settings.gradle.kts
@@ -8,6 +8,6 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "otusKotlinProject"
+rootProject.name = "lessons"
 
-includeBuild("lessons")
+include("m1l1-first")
