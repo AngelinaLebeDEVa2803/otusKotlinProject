7034035f8557c930b940e9998b4380157c991227
AngelinaLebeDEVa2803 <lebedevaangelina2803@gmail.com>
2025-10-04 19:57:42 +0300

    rename

--- deploy/keycloak-tokens.sh
+++ deploy/keycloak-tokens.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 KCHOST=http://localhost:8080
-REALM=otus-marketplace
-CLIENT_ID=otus-marketplace-service
+REALM=otus-smartoffice
+CLIENT_ID=otus-smartoffice-service
 UNAME=otus-test
 PASSWORD=otus
 
