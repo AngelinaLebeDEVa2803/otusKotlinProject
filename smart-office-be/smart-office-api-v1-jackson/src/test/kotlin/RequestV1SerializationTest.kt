44e27a369a79b3ed0625c5f196f4294a57b71bea
Ангелина Лебедева <Angelina.Lebedeva@leroymerlin.ru>
2025-10-31 14:31:03 +0300

    serialize request

--- smart-office-be/smart-office-api-v1-jackson/src/test/kotlin/RequestV1SerializationTest.kt
+++ smart-office-be/smart-office-api-v1-jackson/src/test/kotlin/RequestV1SerializationTest.kt
@@ -5,3 +5,50 @@ import kotlin.test.Test
 import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
+class RequestV1SerializationTest {
+    private val request = BookingCreateRequest(
+        debug = BookingDebug(
+            mode = BookingRequestDebugMode.STUB,
+            stub = BookingRequestDebugStubs.SUCCESS
+        ),
+        booking = BookingCreateObject(
+            userId = "001",
+            floorId = "floor_2",
+            roomId = "418",
+            workspaceId = "007",
+            startTime = "2025-01-10 09:00",
+            endTime = "2025-01-10 18:00",
+            status = BookingStatus.ACTIVE,
+        )
+    )
+
+    @Test
+    fun serialize() {
+        val json = apiV1Mapper.writeValueAsString(request)
+
+        assertContains(json, Regex("\"floorId\":\\s*\"floor_2\""))
+        assertContains(json, Regex("\"workspaceId\":\\s*\"007\""))
+        assertContains(json, Regex("\"endTime\":\\s*\"2025-01-10 18:00\""))
+        assertContains(json, Regex("\"mode\":\\s*\"stub\""))
+        assertContains(json, Regex("\"stub\":\\s*\"success\""))
+        assertContains(json, Regex("\"requestType\":\\s*\"create\""))
+    }
+
+//    @Test
+//    fun deserialize() {
+//        val json = apiV1Mapper.writeValueAsString(request)
+//        val obj = apiV1Mapper.readValue(json, IRequest::class.java) as AdCreateRequest
+//
+//        assertEquals(request, obj)
+//    }
+
+//    @Test
+//    fun deserializeNaked() {
+//        val jsonString = """
+//            {"ad": null}
+//        """.trimIndent()
+//        val obj = apiV1Mapper.readValue(jsonString, AdCreateRequest::class.java)
+//
+//        assertEquals(null, obj.ad)
+//    }
+}
