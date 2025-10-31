7333917099d71fd4b85b58aea6a61b28f3f0ee6a
Ангелина Лебедева <Angelina.Lebedeva@leroymerlin.ru>
2025-10-31 17:31:45 +0300

    stubs

---
+++ smart-office-be/smart-office-common/src/main/kotlin/stubs/OfficeStubs.kt
@@ -0,0 +1,20 @@
+package ru.otus.otuskotlin.smartoffice.common.stubs
+
+enum class OfficeStubs {
+    NONE,
+    SUCCESS,
+    NOT_FOUND,
+    BAD_ID,
+    BAD_USER_ID,
+    BAD_FLOOR_ID,
+    BAD_ROOM_ID,
+    BAD_WORKSPACE_ID,
+    BAD_START_TIME,
+    BAD_END_TIME,
+    BAD_TIME_RANGE,
+    BAD_STATUS,
+    CANNOT_CREATE,
+    CANNOT_UPDATE,
+    CANNOT_DELETE,
+    DB_ERROR, // not in spec
+}
