6030e04c6834b2bf874c13f9976e3ab054a6b66f
Ангелина Лебедева <Angelina.Lebedeva@leroymerlin.ru>
2025-11-05 20:57:01 +0300

    log lib

---
+++ smart-office-libs/smart-office-lib-logging-common/src/main/kotlin/OfficeLoggerProvider.kt
@@ -0,0 +1,38 @@
+package ru.otus.otuskotlin.smartoffice.logging.common
+
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+
+/**
+ * Инициализирует выбранный логер
+ *
+ * ```kotlin
+ * // Обычно логер вызывается вот так
+ * val logger = LoggerFactory.getLogger(this::class.java)
+ * // Мы создаем экземпляр логер-провайдера вот так
+ * val loggerProvider = MkpLoggerProvider { clazz -> mpLoggerLogback(clazz) }
+ *
+ * // В дальнейшем будем использовать этот экземпляр вот так:
+ * val logger = loggerProvider.logger(this::class)
+ * logger.info("My log")
+ * ```
+ */
+class OfficeLoggerProvider(
+    private val provider: (String) -> IOfficeLogWrapper = { IOfficeLogWrapper.DEFAULT }
+) {
+    /**
+     * Инициализирует и возвращает экземпляр логера
+     */
+    fun logger(loggerId: String): IOfficeLogWrapper = provider(loggerId)
+
+    /**
+     * Инициализирует и возвращает экземпляр логера
+     */
+    fun logger(clazz: KClass<*>): IOfficeLogWrapper = provider(clazz.qualifiedName ?: clazz.simpleName ?: "(unknown)")
+
+    /**
+     * Инициализирует и возвращает экземпляр логера
+     */
+    fun logger(function: KFunction<*>): IOfficeLogWrapper = provider(function.name)
+}
+
