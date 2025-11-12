eb2ba704b1f58f73de5b7df14d3f76243bb97597
Ангелина Лебедева <Angelina.Lebedeva@leroymerlin.ru>
2025-11-12 20:06:33 +0300

    biz stubs

---
+++ smart-office-be/smart-office-biz/src/main/kotlin/stubs/StubAllSuccess.kt
@@ -0,0 +1,25 @@
+package ru.otus.otuskotlin.smartoffice.biz.stubs
+
+import ru.otus.otuskotlin.smartoffice.cor.ICorChainDsl
+import ru.otus.otuskotlin.smartoffice.cor.worker
+import ru.otus.otuskotlin.smartoffice.common.OfficeContext
+import ru.otus.otuskotlin.smartoffice.common.OfficeCorSettings
+import ru.otus.otuskotlin.smartoffice.common.models.*
+import ru.otus.otuskotlin.smartoffice.common.stubs.OfficeStubs
+import ru.otus.otuskotlin.smartoffice.logging.common.LogLevel
+import ru.otus.otuskotlin.smartoffice.stubs.OfficeBookingStub
+
+fun ICorChainDsl<OfficeContext>.stubAllSuccess(title: String, corSettings: OfficeCorSettings) = worker {
+    this.title = title
+    this.description = """
+        Кейс успеха для отображения всех бронирований пользователя
+    """.trimIndent()
+    on { stubCase == OfficeStubs.SUCCESS && state == OfficeState.RUNNING }
+    val logger = corSettings.loggerProvider.logger("stubAllSuccess")
+    handle {
+        logger.doWithLogging(id = this.requestId.asString(), LogLevel.DEBUG) {
+            state = OfficeState.FINISHING
+            bookingsResponse.addAll(OfficeBookingStub.prepareBookingsList(bookingFilterRequest.searchString, MkplDealSide.DEMAND))
+        }
+    }
+}
