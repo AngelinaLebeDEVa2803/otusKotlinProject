14260506a602fde7585027a1b206c9c83265e9ec
Ангелина Лебедева <Angelina.Lebedeva@leroymerlin.ru>
2025-11-13 15:10:42 +0300

    biz stubs test

--- smart-office-be/smart-office-biz/src/test/kotlin/stubs/BookingCreateStubTest.kt
+++ smart-office-be/smart-office-biz/src/test/kotlin/stubs/BookingCreateStubTest.kt
@@ -13,7 +13,7 @@ import kotlin.test.assertEquals
 class BookingCreateStubTest {
 
     private val processor = OfficeBookingProcessor()
-    val bookingId = OfficeBookingId("001")
+    val bookingId = OfficeBookingId("00100")
     val userId = OfficeUserId("123")
     val floorId = OfficeFloorId("floor_6")
     val roomId = OfficeRoomId("room_404")
@@ -53,81 +53,266 @@ class BookingCreateStubTest {
         assertEquals(status, ctx.bookingResponse.status)
     }
 
-//    @Test
-//    fun badTitle() = runTest {
-//        val ctx = MkplContext(
-//            command = MkplCommand.CREATE,
-//            state = MkplState.NONE,
-//            workMode = MkplWorkMode.STUB,
-//            stubCase = MkplStubs.BAD_TITLE,
-//            adRequest = MkplAd(
-//                id = id,
-//                title = "",
-//                description = description,
-//                adType = dealSide,
-//                visibility = visibility,
-//            ),
-//        )
-//        processor.exec(ctx)
-//        assertEquals(MkplAd(), ctx.adResponse)
-//        assertEquals("title", ctx.errors.firstOrNull()?.field)
-//        assertEquals("validation", ctx.errors.firstOrNull()?.group)
-//    }
-//    @Test
-//    fun badDescription() = runTest {
-//        val ctx = MkplContext(
-//            command = MkplCommand.CREATE,
-//            state = MkplState.NONE,
-//            workMode = MkplWorkMode.STUB,
-//            stubCase = MkplStubs.BAD_DESCRIPTION,
-//            adRequest = MkplAd(
-//                id = id,
-//                title = title,
-//                description = "",
-//                adType = dealSide,
-//                visibility = visibility,
-//            ),
-//        )
-//        processor.exec(ctx)
-//        assertEquals(MkplAd(), ctx.adResponse)
-//        assertEquals("description", ctx.errors.firstOrNull()?.field)
-//        assertEquals("validation", ctx.errors.firstOrNull()?.group)
-//    }
+    @Test
+    fun badUserId() = runTest {
+        val ctx = OfficeContext(
+            command = OfficeCommand.CREATE,
+            state = OfficeState.NONE,
+            workMode = OfficeWorkMode.STUB,
+            stubCase = OfficeStubs.BAD_USER_ID,
+            bookingRequest = OfficeBooking(
+                id = bookingId,
+                userId = userId,
+                floorId = floorId,
+                roomId = roomId,
+                workspaceId = workspaceId,
+                startTime = startTime,
+                endTime = endTime,
+                status = status,
+            ),
+        )
+        processor.exec(ctx)
+        assertEquals(OfficeBooking(), ctx.bookingResponse)
+        assertEquals("userId", ctx.errors.firstOrNull()?.field)
+        assertEquals("validation", ctx.errors.firstOrNull()?.group)
+    }
 
-//    @Test
-//    fun databaseError() = runTest {
-//        val ctx = MkplContext(
-//            command = MkplCommand.CREATE,
-//            state = MkplState.NONE,
-//            workMode = MkplWorkMode.STUB,
-//            stubCase = MkplStubs.DB_ERROR,
-//            adRequest = MkplAd(
-//                id = id,
-//            ),
-//        )
-//        processor.exec(ctx)
-//        assertEquals(MkplAd(), ctx.adResponse)
-//        assertEquals("internal", ctx.errors.firstOrNull()?.group)
-//    }
+    @Test
+    fun badFloorId() = runTest {
+        val ctx = OfficeContext(
+            command = OfficeCommand.CREATE,
+            state = OfficeState.NONE,
+            workMode = OfficeWorkMode.STUB,
+            stubCase = OfficeStubs.BAD_FLOOR_ID,
+            bookingRequest = OfficeBooking(
+                id = bookingId,
+                userId = userId,
+                floorId = floorId,
+                roomId = roomId,
+                workspaceId = workspaceId,
+                startTime = startTime,
+                endTime = endTime,
+                status = status,
+            ),
+        )
+        processor.exec(ctx)
+        assertEquals(OfficeBooking(), ctx.bookingResponse)
+        assertEquals("floorId", ctx.errors.firstOrNull()?.field)
+        assertEquals("validation", ctx.errors.firstOrNull()?.group)
+    }
 
-//    @Test
-//    fun badNoCase() = runTest {
-//        val ctx = MkplContext(
-//            command = MkplCommand.CREATE,
-//            state = MkplState.NONE,
-//            workMode = MkplWorkMode.STUB,
-//            stubCase = MkplStubs.BAD_ID,
-//            adRequest = MkplAd(
-//                id = id,
-//                title = title,
-//                description = description,
-//                adType = dealSide,
-//                visibility = visibility,
-//            ),
-//        )
-//        processor.exec(ctx)
-//        assertEquals(MkplAd(), ctx.adResponse)
-//        assertEquals("stub", ctx.errors.firstOrNull()?.field)
-//        assertEquals("validation", ctx.errors.firstOrNull()?.group)
-//    }
+    @Test
+    fun badRoomId() = runTest {
+        val ctx = OfficeContext(
+            command = OfficeCommand.CREATE,
+            state = OfficeState.NONE,
+            workMode = OfficeWorkMode.STUB,
+            stubCase = OfficeStubs.BAD_ROOM_ID,
+            bookingRequest = OfficeBooking(
+                id = bookingId,
+                userId = userId,
+                floorId = floorId,
+                roomId = roomId,
+                workspaceId = workspaceId,
+                startTime = startTime,
+                endTime = endTime,
+                status = status,
+            ),
+        )
+        processor.exec(ctx)
+        assertEquals(OfficeBooking(), ctx.bookingResponse)
+        assertEquals("roomId", ctx.errors.firstOrNull()?.field)
+        assertEquals("validation", ctx.errors.firstOrNull()?.group)
+    }
+
+    @Test
+    fun badWorkspaceId() = runTest {
+        val ctx = OfficeContext(
+            command = OfficeCommand.CREATE,
+            state = OfficeState.NONE,
+            workMode = OfficeWorkMode.STUB,
+            stubCase = OfficeStubs.BAD_WORKSPACE_ID,
+            bookingRequest = OfficeBooking(
+                id = bookingId,
+                userId = userId,
+                floorId = floorId,
+                roomId = roomId,
+                workspaceId = workspaceId,
+                startTime = startTime,
+                endTime = endTime,
+                status = status,
+            ),
+        )
+        processor.exec(ctx)
+        assertEquals(OfficeBooking(), ctx.bookingResponse)
+        assertEquals("workspaceId", ctx.errors.firstOrNull()?.field)
+        assertEquals("validation", ctx.errors.firstOrNull()?.group)
+    }
+
+    @Test
+    fun badStartTime() = runTest {
+        val ctx = OfficeContext(
+            command = OfficeCommand.CREATE,
+            state = OfficeState.NONE,
+            workMode = OfficeWorkMode.STUB,
+            stubCase = OfficeStubs.BAD_START_TIME,
+            bookingRequest = OfficeBooking(
+                id = bookingId,
+                userId = userId,
+                floorId = floorId,
+                roomId = roomId,
+                workspaceId = workspaceId,
+                startTime = startTime,
+                endTime = endTime,
+                status = status,
+            ),
+        )
+        processor.exec(ctx)
+        assertEquals(OfficeBooking(), ctx.bookingResponse)
+        assertEquals("startTime", ctx.errors.firstOrNull()?.field)
+        assertEquals("validation", ctx.errors.firstOrNull()?.group)
+    }
+
+    @Test
+    fun badEndTime() = runTest {
+        val ctx = OfficeContext(
+            command = OfficeCommand.CREATE,
+            state = OfficeState.NONE,
+            workMode = OfficeWorkMode.STUB,
+            stubCase = OfficeStubs.BAD_END_TIME,
+            bookingRequest = OfficeBooking(
+                id = bookingId,
+                userId = userId,
+                floorId = floorId,
+                roomId = roomId,
+                workspaceId = workspaceId,
+                startTime = startTime,
+                endTime = endTime,
+                status = status,
+            ),
+        )
+        processor.exec(ctx)
+        assertEquals(OfficeBooking(), ctx.bookingResponse)
+        assertEquals("endTime", ctx.errors.firstOrNull()?.field)
+        assertEquals("validation", ctx.errors.firstOrNull()?.group)
+    }
+
+    @Test
+    fun badTimeRange() = runTest {
+        val ctx = OfficeContext(
+            command = OfficeCommand.CREATE,
+            state = OfficeState.NONE,
+            workMode = OfficeWorkMode.STUB,
+            stubCase = OfficeStubs.BAD_TIME_RANGE,
+            bookingRequest = OfficeBooking(
+                id = bookingId,
+                userId = userId,
+                floorId = floorId,
+                roomId = roomId,
+                workspaceId = workspaceId,
+                startTime = startTime,
+                endTime = endTime,
+                status = status,
+            ),
+        )
+        processor.exec(ctx)
+        assertEquals(OfficeBooking(), ctx.bookingResponse)
+        assertEquals("validation-timeRange", ctx.errors.firstOrNull()?.code)
+        assertEquals("validation", ctx.errors.firstOrNull()?.group)
+    }
+
+    @Test
+    fun badStatus() = runTest {
+        val ctx = OfficeContext(
+            command = OfficeCommand.CREATE,
+            state = OfficeState.NONE,
+            workMode = OfficeWorkMode.STUB,
+            stubCase = OfficeStubs.BAD_STATUS,
+            bookingRequest = OfficeBooking(
+                id = bookingId,
+                userId = userId,
+                floorId = floorId,
+                roomId = roomId,
+                workspaceId = workspaceId,
+                startTime = startTime,
+                endTime = endTime,
+                status = status,
+            ),
+        )
+        processor.exec(ctx)
+        assertEquals(OfficeBooking(), ctx.bookingResponse)
+        assertEquals("status", ctx.errors.firstOrNull()?.field)
+        assertEquals("validation", ctx.errors.firstOrNull()?.group)
+    }
+
+    @Test
+    fun stubTestCannotCreate() = runTest {
+        val ctx = OfficeContext(
+            command = OfficeCommand.CREATE,
+            state = OfficeState.NONE,
+            workMode = OfficeWorkMode.STUB,
+            stubCase = OfficeStubs.CANNOT_CREATE,
+            bookingRequest = OfficeBooking(
+                id = bookingId,
+                userId = userId,
+                floorId = floorId,
+                roomId = roomId,
+                workspaceId = workspaceId,
+                startTime = startTime,
+                endTime = endTime,
+                status = status,
+            ),
+        )
+        processor.exec(ctx)
+        assertEquals(OfficeBooking(), ctx.bookingResponse)
+        assertEquals("logic-cannotCreate", ctx.errors.firstOrNull()?.code)
+        assertEquals("logic", ctx.errors.firstOrNull()?.group)
+    }
+
+    @Test
+    fun databaseError() = runTest {
+        val ctx = OfficeContext(
+            command = OfficeCommand.CREATE,
+            state = OfficeState.NONE,
+            workMode = OfficeWorkMode.STUB,
+            stubCase = OfficeStubs.DB_ERROR,
+            bookingRequest = OfficeBooking(
+                id = bookingId,
+                userId = userId,
+                floorId = floorId,
+                roomId = roomId,
+                workspaceId = workspaceId,
+                startTime = startTime,
+                endTime = endTime,
+                status = status,
+            ),
+        )
+        processor.exec(ctx)
+        assertEquals(OfficeBooking(), ctx.bookingResponse)
+        assertEquals("internal", ctx.errors.firstOrNull()?.group)
+    }
+
+    @Test
+    fun badNoCase() = runTest {
+        val ctx = OfficeContext(
+            command = OfficeCommand.CREATE,
+            state = OfficeState.NONE,
+            workMode = OfficeWorkMode.STUB,
+            stubCase = OfficeStubs.CANNOT_UPDATE,
+            bookingRequest = OfficeBooking(
+                id = bookingId,
+                userId = userId,
+                floorId = floorId,
+                roomId = roomId,
+                workspaceId = workspaceId,
+                startTime = startTime,
+                endTime = endTime,
+                status = status,
+            ),
+        )
+        processor.exec(ctx)
+        assertEquals(OfficeBooking(), ctx.bookingResponse)
+        assertEquals("stub", ctx.errors.firstOrNull()?.field)
+        assertEquals("validation", ctx.errors.firstOrNull()?.group)
+    }
 }
