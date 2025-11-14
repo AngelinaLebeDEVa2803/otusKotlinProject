a5f70ce14a080138a3121e019940dd474589aea1
Ангелина Лебедева <Angelina.Lebedeva@leroymerlin.ru>
2025-11-14 18:33:35 +0300

    biz validation ref

--- smart-office-be/smart-office-biz/src/main/kotlin/validation/ValidateTimeRangeBooking.kt
+++ smart-office-be/smart-office-biz/src/main/kotlin/validation/ValidateTimeRangeBooking.kt
@@ -51,7 +51,7 @@ fun ICorChainDsl<OfficeContext>.validateTimeRangeBooking(title: String) = chain 
 fun ICorChainDsl<OfficeContext>.validateTimeRange(title: String) = worker {
     this.title = title
 
-    on { bookingFilterValidating.startTime > bookingFilterValidating.endTime }
+    on { bookingValidating.startTime > bookingValidating.endTime }
     handle {
         fail(
             errorValidation(
