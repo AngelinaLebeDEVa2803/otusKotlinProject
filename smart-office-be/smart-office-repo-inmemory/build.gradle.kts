4fda29701d53091f17c4be1138bdd4222dbf633f
Ангелина Лебедева <Angelina.Lebedeva@leroymerlin.ru>
2025-11-19 19:19:10 +0300

    repo inmemory

---
+++ smart-office-be/smart-office-repo-inmemory/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("build-jvm")
+}
+
+group = rootProject.group
+version = rootProject.version
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(projects.smartOfficeCommon)
+    implementation(projects.smartOfficeRepoCommon)
+
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.core)
+    implementation(libs.db.cache4k)
+    implementation(libs.uuid)
+
+
+    testImplementation(kotlin("test-junit"))
+    testImplementation(projects.smartOfficeStubs)
+    api(libs.coroutines.test)
+}
