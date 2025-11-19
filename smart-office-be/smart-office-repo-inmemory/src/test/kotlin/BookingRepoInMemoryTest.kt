7b84d8de6f923e54ae16fe0540cf82a9c961fc20
Ангелина Лебедева <Angelina.Lebedeva@leroymerlin.ru>
2025-11-19 21:53:34 +0300

    repo inmemory

---
+++ smart-office-be/smart-office-repo-inmemory/src/test/kotlin/BookingRepoInMemoryTest.kt
@@ -0,0 +1,38 @@
+import ru.otus.otuskotlin.smartoffice.backend.repo.tests.*
+import ru.otus.otuskotlin.smartoffice.repo.common.BookingRepoInitialized
+import ru.otus.otuskotlin.smartoffice.repo.inmemory.BookingRepoInMemory
+
+class BookingRepoInMemoryCreateTest : RepoBookingCreateTest() {
+    override val repo = BookingRepoInitialized(
+        BookingRepoInMemory(randomUuid = { uuidNew.asString() }),
+        initObjects = initObjects,
+    )
+}
+
+class BookingRepoInMemoryDeleteTest : RepoBookingDeleteTest() {
+    override val repo = BookingRepoInitialized(
+        BookingRepoInMemory(),
+        initObjects = initObjects,
+    )
+}
+
+class BookingRepoInMemoryReadTest : RepoBookingReadTest() {
+    override val repo = BookingRepoInitialized(
+        BookingRepoInMemory(),
+        initObjects = initObjects,
+    )
+}
+
+class BookingRepoInMemoryAllTest : RepoBookingAllTest() {
+    override val repo = BookingRepoInitialized(
+        BookingRepoInMemory(),
+        initObjects = initObjects,
+    )
+}
+
+class BookingRepoInMemoryUpdateTest : RepoBookingUpdateTest() {
+    override val repo = BookingRepoInitialized(
+        BookingRepoInMemory(randomUuid = { lockNew.asString() }),
+        initObjects = initObjects,
+    )
+}
